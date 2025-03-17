6d20a70fbe655b1b96b5ea58e4b9eac497c052e3
tossyKim <dydrkfk22@gmail.com>
2025-03-17 16:27:41 +0900

    EditText에 입력받아서 처리 onclick

--- app/src/main/java/kr/ac/kopo/helloandroid/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/helloandroid/MainActivity.java
@@ -1,9 +1,15 @@
 package kr.ac.kopo.helloandroid;
 
 import android.os.Bundle;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -15,10 +21,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.icon);
+        Button btn1 = (Button) findViewById(R.id.btn1);
+        TextView tv1 = (TextView) findViewById(R.id.tv1);
+        EditText edit1 = findViewById(R.id.editname);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "버튼을 클릭하셨네요.", Toast.LENGTH_LONG);
+                toast.show();
+                String name = edit1.getText().toString();
+                tv1.setText(name + " 입니다.");
+            }
         });
+
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
     }
 }
